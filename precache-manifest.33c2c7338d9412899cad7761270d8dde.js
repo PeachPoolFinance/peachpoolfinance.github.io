366272cc363197fa1e78901b7347c9d67528eefd
PeachPool <peachpool@protonmail.com>
2020-09-30 16:01:01 -0400

    update

---
+++ precache-manifest.33c2c7338d9412899cad7761270d8dde.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17a91950f3dd5df93882858a98af486",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb015ab1bd3bcc9b457e",
+    "url": "/static/css/main.977a8089.chunk.css"
+  },
+  {
+    "revision": "ead6affcd4615f52f7ba",
+    "url": "/static/js/2.469fa8bc.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.469fa8bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb015ab1bd3bcc9b457e",
+    "url": "/static/js/main.73478796.chunk.js"
+  },
+  {
+    "revision": "04f38e7ea3f0ab30c1b5",
+    "url": "/static/js/runtime-main.a2e873e0.js"
+  },
+  {
+    "revision": "ed6ef54643d76e7111b2a63f2193a83b",
+    "url": "/static/media/BadUnicorn-Regular.ed6ef546.ttf"
+  },
+  {
+    "revision": "f90ddea5052a7b14f994e2b7bdd60954",
+    "url": "/static/media/close.f90ddea5.svg"
+  },
+  {
+    "revision": "36e2112aa7bd47bc0454da1cd2b5a5cd",
+    "url": "/static/media/copy.36e2112a.svg"
+  },
+  {
+    "revision": "ca2a682f9c6977a8adda0a10d84753da",
+    "url": "/static/media/github.ca2a682f.svg"
+  },
+  {
+    "revision": "99a7fc31a0e55bcb30e1fadfd0a2a4ef",
+    "url": "/static/media/hamburger.99a7fc31.svg"
+  },
+  {
+    "revision": "3473cffcf0c74d681e79030e34662d7d",
+    "url": "/static/media/link.3473cffc.svg"
+  },
+  {
+    "revision": "09c838e15ca4ec8b2c9f7bdfe322d54b",
+    "url": "/static/media/medium.09c838e1.svg"
+  },
+  {
+    "revision": "455f276dd3ffb496d91f43492fc2722c",
+    "url": "/static/media/peachpool-logo-jumbotron.455f276d.png"
+  },
+  {
+    "revision": "43af099a2e39108fc20d7ed1712c68c9",
+    "url": "/static/media/telegram.43af099a.svg"
+  },
+  {
+    "revision": "f9ab500877ef83e0e4473a28ce6bdd0b",
+    "url": "/static/media/twitter.f9ab5008.svg"
+  }
+]);
